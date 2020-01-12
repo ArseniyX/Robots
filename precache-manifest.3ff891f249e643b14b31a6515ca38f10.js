cf81aa1e1ea47ac1c245a50f3a13884b6f85befa
ArseniyX <jkl@usa.com>
2020-01-12 13:59:50 +0200

    Initial commit

---
+++ precache-manifest.3ff891f249e643b14b31a6515ca38f10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112e6cc14d917678168cfab74f09c51e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c9dbeef573817c143e9",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "30bc06a842352011adba",
+    "url": "/static/css/main.8f672258.chunk.css"
+  },
+  {
+    "revision": "7c9dbeef573817c143e9",
+    "url": "/static/js/2.3a77dedc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3a77dedc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30bc06a842352011adba",
+    "url": "/static/js/main.4c52af3f.chunk.js"
+  },
+  {
+    "revision": "233e64e2b074943728ef",
+    "url": "/static/js/runtime-main.cff0e806.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
